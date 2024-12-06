79b5465620cca6ce13aeedc8fe60e942f0390c20
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-12-06 16:37:59 +0900

    feat:asyncStorage.getData 제네릭 추가하여 반환하는 데이터 타입 알수있도록

--- app/utils/asyncStorage.ts
+++ app/utils/asyncStorage.ts
@@ -1,23 +1,25 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+type AsyncStorageDataType<T> = T extends object ? T : string;
+
 export const asyncStorage = {
-  storeData : async (key:string, value:any) => {
+  storeData: async(key:string, value:any) => {
     try {
       await AsyncStorage.setItem(key, typeof value === 'object' ? JSON.stringify(value) : value);
     } catch (e) {
     }
   },
-   getData : async (key:string) => {
+   getData : async <T>(key:string):Promise<AsyncStorageDataType<T> | null> => {
     try {
       const value = await AsyncStorage.getItem(key);
-      if(value === null){
-        return null;
-      }
-      if(typeof value === 'object'){
-        return JSON.parse(value);
-      }
-      return value;
+      if(typeof value === 'string'){
+        const parsedValue = JSON.parse(value);
+          return parsedValue;
+      }  
+      return null;
     } catch (e) {
+      const value = await AsyncStorage.getItem(key);
+      return value as AsyncStorageDataType<T>;
     }
   }
 }
