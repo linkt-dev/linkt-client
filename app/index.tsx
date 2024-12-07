14cf0b3e5b8516c153945bff3cdcce9e27f58051
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-12-07 20:14:01 +0900

    feat:push notification 테스트

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,11 @@
+import PushNotification from "@/push-notification";
 import WebView from "react-native-webview";
 
 export default function HomeScreen() {
-  return <WebView source={{ uri: "https://linkt-webview.vercel.app/" }} />;
+  return (
+    <>
+      <PushNotification />
+      <WebView source={{ uri: "https://linkt-webview.vercel.app/" }} />;
+    </>
+  );
 }
