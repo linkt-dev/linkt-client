865032ee2e5f1a5e3ad3ade01f55b2129396ef78
오현재 <55138387+hangooksaram@users.noreply.github.com>
2024-12-19 15:44:22 +0900

    style:push notification 예제 코드 제거

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,5 @@
-import PushNotification from "@/push-notification";
 import WebView from "react-native-webview";
 
 export default function HomeScreen() {
-  return (
-    <>
-      <PushNotification />
-      <WebView source={{ uri: "https://linkt-webview.vercel.app/" }} />
-    </>
-  );
+  return <WebView source={{ uri: "https://linkt-webview.vercel.app/" }} />;
 }
