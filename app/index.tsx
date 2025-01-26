49d803b3089225974e0b5a321b69f100746a23ef
오현재 <55138387+hangooksaram@users.noreply.github.com>
2025-01-26 20:09:17 +0900

    feat:android 에도 safe area view 적용되도록

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,15 @@
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, StatusBar, Platform } from "react-native";
 import WebView from "react-native-webview";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "red" }}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+        backgroundColor: "white",
+      }}
+    >
       <WebView source={{ uri: "https://linkt.one" }} />
     </SafeAreaView>
   );
