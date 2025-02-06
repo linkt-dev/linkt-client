69c11d60a289b13a68bf681b4bc10bf1ecb5d0de
오현재 <55138387+hangooksaram@users.noreply.github.com>
2025-02-06 22:51:47 +0900

    fix:오탈자 제거

--- app/index.tsx
+++ app/index.tsx
@@ -21,7 +21,6 @@ export default function HomeScreen() {
     >
       <WebView
         source={{ uri: "https://linkt.one" }}
-        runFirst
         injectedJavaScript={runFirst}
       />
     </SafeAreaView>
